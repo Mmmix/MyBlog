ff11763053cd770b4b3ff77539df93b5d4b9a7bb
jiaxin.m <jiaxin.m@baisonmail.com>
2019-03-13 15:09:57 +0800

    原本的启动类中，继承SpringBootServletInitializer,然后重写configure方法

--- src/main/java/com/wust/myblog/MyblogApplication.java
+++ src/main/java/com/wust/myblog/MyblogApplication.java
@@ -3,13 +3,21 @@ package com.wust.myblog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @ComponentScan("com.wust")
 @MapperScan("com.wust.myblog.mapper")
 @SpringBootApplication
-public class MyblogApplication {
+public class MyblogApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder app){
+        return app.sources(MyblogApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(MyblogApplication.class, args);
     }
